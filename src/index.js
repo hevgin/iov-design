5d38def945c166f6696910f056125af5002689ca
guanghuangjin <guanghj@chinaunicom.cn>
2025-11-10 17:12:45 +0800

    feat: 将文件图标移到公共位置

--- src/index.js
+++ src/index.js
@@ -212,7 +212,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.15.75',
+  version: '2.15.76',
   locale: locale.use,
   i18n: locale.i18n,
   install,
---
+++ src/utils/file-icon.js
@@ -0,0 +1,11 @@
+export default {
+  img: '//obs-helf.cucloud.cn/prod-common-public/iov-design/img.png', // require('./assets/img.png'),
+  pdf: '//obs-helf.cucloud.cn/prod-common-public/iov-design/pdf.png', // require('./assets/pdf.png'),
+  excel: '//obs-helf.cucloud.cn/prod-common-public/iov-design/excel.png', // require('./assets/excel.png'),
+  zip: '//obs-helf.cucloud.cn/prod-common-public/iov-design/zip.png', // require('./assets/zip.png'),
+  rar: '//obs-helf.cucloud.cn/prod-common-public/iov-design/zip.png', // require('./assets/zip.png'),
+  '7z': '//obs-helf.cucloud.cn/prod-common-public/iov-design/zip.png', // require('./assets/zip.png'),
+  ppt: '//obs-helf.cucloud.cn/prod-common-public/iov-design/ppt.png', // require('./assets/ppt.png'),
+  word: '//obs-helf.cucloud.cn/prod-common-public/iov-design/word.png', // require('./assets/word.png'),
+  default: '//obs-helf.cucloud.cn/prod-common-public/iov-design/file.png' // require('./assets/file.png')
+};
